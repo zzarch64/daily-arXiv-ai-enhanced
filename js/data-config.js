81996d0200908a14d5eb7888b76aee6de4a62755
Andrew <zzarch64@gmail.com>
2026-07-28 18:09:56 +0800

    chore: configure fork data source

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'dw-dengwei',
+    repoOwner: 'zzarch64',
 
     /**
      * GitHub repository name
@@ -43,4 +43,3 @@ const DATA_CONFIG = {
         return `${this.getDataBaseUrl()}/${filePath}`;
     }
 };
-
